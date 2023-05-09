ebaccb87bc33e9556a03c700e9869fed49277afd
Bradley Shilton <bradyshilton@gmail.com>
2023-05-09 21:11:46 +0300

    Done finished my glide js carousel template

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+const config  = {
+  type: 'carousel',
+  perView: 3,
+  focusAt: 'center',
+  startAt: '1',
+  breakpoints: {
+    800: {
+      perView: 2,
+    },
 
+    420: {
+      perView: 1
+    }
+  }
+
+}
+
+new Glide('.glide', config).mount()
